9fdf269bcf023f6190151f71dcef3caf608a9ee2
TuNguyen-1104 <72005427+TuNguyen-1104@users.noreply.github.com>
2020-09-28 14:03:07 +0700

    Add files via upload

---
+++ Helloworld1/Helloworld1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helloworld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HELLLO WORLD");
+            Console.ReadKey();
+        }
+    }
+}
